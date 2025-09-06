5b26cc0b3754590dd31083f557948869c99ecc1a
NIKHIL KATHAIT <nknick97@gmail.com>
2025-09-06 17:30:13 +0530

    Optimized solution for countingIncreasingTriplets2DArray

--- src/countingIncreasingTriplets2DArray.java
+++ src/countingIncreasingTriplets2DArray.java
@@ -3,16 +3,26 @@ public class countingIncreasingTriplets2DArray {
         int N = A.length;
         int count = 0;
 
-        for(int i = 0; i< N-2; i++)
+        for(int j = 1; j< N-1; j++)
         {
-            for(int j = 1; j< N-1; j++)
+            int leftCount = 0, rightCount = 0;
+
+            // count elements less than A[j] on left
+            for(int i = 0; i<j; i++)
             {
-                for(int k = 2; k<N; k++)
-                {
-                    if((i<j && j<k) && (A[i]<A[j] && A[j]<A[k]))
-                        count++;
-                }
+                if(A[i] < A[j])
+                    leftCount++;
             }
+
+            // count elements greater than A[j] on right
+            for(int k = j + 1; k < N; k++)
+            {
+                if(A[k] > A[j])
+                    rightCount++;
+            }
+
+            // total triplets with j as middle element
+            count += leftCount * rightCount;
         }
 
         return count;
